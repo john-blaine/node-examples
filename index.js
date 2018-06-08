dc110836a107021f67472473d313ee2494c9f085
John Blaine <jablaine@uw.edu>
2018-06-08 08:08:44 -0600

    Refactor https request to log response object and take variable id

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const https = require('https');
+const testId = 1;
 
 const printComments = (name, email, comment) => {
     const message = `User ${name} with the email ${email} left the following comment:\n${comment}`;
@@ -7,7 +8,8 @@ const printComments = (name, email, comment) => {
 
 printComments('John', 'john.alan.blaine@gmail.com', 'This is a comment');
 
-https.get('https://jsonplaceholder.typicode.com/posts/1', (res) => {
+https.get(`https://jsonplaceholder.typicode.com/posts/${testId}`, (res) => {
     console.log('statusCode:', res.statusCode);
+    console.dir(res);
 })
 
