38747d24fb89e2b2b8d6d39ecb66b4c8ab5c22e0
John Blaine <jablaine@uw.edu>
2018-06-08 07:41:55 -0600

    Create function for manipulating data from API endpoint

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 console.log('Hello world');
 console.error('Error 404');
-console.dir({name: John, age: 33});
+console.dir({name: 'John', age: 33});
+
+const printComments = (name, email, comment) => {
+    const message = `User ${name} with the email ${email} left the following comment:\n${comment}`;
+    console.log(message);
+}
+
+printComments('John', 'john.alan.blaine@gmail.com', 'This is a comment');
