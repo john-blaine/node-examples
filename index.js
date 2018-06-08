b872c1dec79788656900a0aa1313752aa0e0b5ab
John Blaine <jablaine@uw.edu>
2018-06-08 11:37:56 -0600

    Refactor http get request into a function, remove testId const

--- index.js
+++ index.js
@@ -1,25 +1,28 @@
 const https = require('https');
-const testId = 1;
 
 const printComments = (name, email, comment) => {
     const message = `User ${name} with the email ${email} left the following comment:\n${comment}`;
     console.log(message);
 }
 
-https.get(`https://jsonplaceholder.typicode.com/comments?postId=${testId}`, (res) => {
-    console.log('statusCode:', res.statusCode);
-
-    let body = '';
-
-    res.on('data', data => {
-        body += data.toString();
+function getComments(testId) {
+    https.get(`https://jsonplaceholder.typicode.com/comments?postId=${testId}`, (res) => {
+        console.log('statusCode:', res.statusCode);
+    
+        let body = '';
+    
+        res.on('data', data => {
+            body += data.toString();
+        });
+    
+        res.on('end', () => {
+            body = JSON.parse(body);
+            for (var i = 0; i < body.length; i++) {
+              printComments(body[i].name, body[i].email, body[i].body);
+            }
+        })
     });
+}
 
-    res.on('end', () => {
-        body = JSON.parse(body);
-        for (var i = 0; i < body.length; i++) {
-          printComments(body[i].name, body[i].email, body[i].body);
-        }
-    })
-});
+
 
