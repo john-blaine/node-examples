6967ba945e02d520e2880e3393667ee32636c1ce
John Blaine <jablaine@uw.edu>
2018-06-13 16:02:35 -0600

    Add request and address to getWeather, create var for API key

--- weatherCmdLn/index.js
+++ weatherCmdLn/index.js
@@ -1,12 +1,14 @@
 const https = require('https');
 
+const apiKey = process.API_KEY;
+
 const printTemp = (city, temp) => {
     const message = `Current temperature in ${city} is ${temp}F`;
     console.log(message);
 }
 
 const getWeather = (zipOrCity) => {
-  console.log(zipOrCity);
+  const request = https.get(`http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=${apiKey}`);
 };
 
 getWeather(process.argv[2]);
