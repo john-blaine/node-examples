2bb29805f668327e197f668708c87ba95501c467
John Blaine <jablaine@uw.edu>
2018-06-13 15:54:12 -0600

    Add helper function for printing temp in city to console

--- weatherCmdLn/index.js
+++ weatherCmdLn/index.js
@@ -1,5 +1,10 @@
 const https = require('https');
 
+const printTemp = (city, temp) => {
+    const message = `Current temperature in ${city} is ${temp}F`;
+    console.log(message);
+}
+
 const getWeather = (zipOrCity) => {
   console.log(zipOrCity);
 };
