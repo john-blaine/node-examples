f8ceb410f0065df9363b88d9be29e20010c9e861
John Blaine <jablaine@uw.edu>
2018-06-14 11:01:08 -0600

    Create regex conditional to test if argV is zip or city

--- weatherCmdLn/index.js
+++ weatherCmdLn/index.js
@@ -4,11 +4,18 @@ const http = require('http');
 const apiKey = process.env.API_KEY;
 
 const printTemp = (city, temp) => {
-    const message = `Current temperature in ${city} is ${temp}F`;
-    console.log(message);
+  const message = `Current temperature in ${city} is ${temp}F`;
+  console.log(message);
 }
 
 const getWeather = (zipOrCity) => {
+
+  if (zipOrCity.match(/(^\d{5}$)/gm)) {
+    console.log('Matched');
+  } else {
+    console.log('No match');
+  }
+
   const request = http.get(`http://api.openweathermap.org/data/2.5/find?q=London&units=imperial&APPID=${apiKey}`, (res) => {
     let body = '';
 
