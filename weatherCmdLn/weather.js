9309f7f2f844280459f5c6120321fddb24308049
John Blaine <jablaine@uw.edu>
2018-06-15 07:01:52 -0600

    Add error handling for http request

--- weatherCmdLn/weather.js
+++ weatherCmdLn/weather.js
@@ -8,28 +8,37 @@ const printTemp = (city, temp) => {
   console.log(message);
 }
 
+const printError = (error) => {
+    console.error(error.message);
+}
+
 const getWeather = (zipOrCity) => {
+    try {
+        let query;
 
-  let query;
+        if (zipOrCity.match(/(^\d{5}$)/gm)) {
+          query = `http://api.openweathermap.org/data/2.5/weather?zip=${zipOrCity}&units=imperial&APPID=${apiKey}`;
+        } else {
+          query = `http://api.openweathermap.org/data/2.5/weather?q=${zipOrCity}&units=imperial&APPID=${apiKey}`;
+        }
+      
+        const request = http.get(query, (res) => {
+          let body = '';
+      
+          res.on('data', data => {
+            body += data.toString();
+          })
+      
+          res.on('end', () => {
+            body = JSON.parse(body);
+            printTemp(body.name, Math.round(body.main.temp * 10) / 10);
+          })
+        });
 
-  if (zipOrCity.match(/(^\d{5}$)/gm)) {
-    query = `http://api.openweathermap.org/data/2.5/weather?zip=${zipOrCity}&units=imperial&APPID=${apiKey}`;
-  } else {
-    query = `http://api.openweathermap.org/data/2.5/weather?q=${zipOrCity}&units=imperial&APPID=${apiKey}`;
-  }
-
-  const request = http.get(query, (res) => {
-    let body = '';
-
-    res.on('data', data => {
-      body += data.toString();
-    })
-
-    res.on('end', () => {
-      body = JSON.parse(body);
-      printTemp(body.name, Math.round(body.main.temp * 10) / 10);
-    })
-  });
+        request.on('error', error => console.error(`Problem with request: ${error.message}`))
+    } catch (error) {
+        printError(error);
+    }
 };
 
 module.exports = getWeather;
