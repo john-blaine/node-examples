3c7b458d8ad5281ed2d2c2a29ee803365afe0c7f
John Blaine <jablaine@uw.edu>
2018-06-15 07:48:53 -0600

    Refactor response error handling, change all tabs to 2 spaces
    To handle all responses that aren't 200 to increase error coverage
    and keep as much information to report to the user as possible

--- weatherCmdLn/weather.js
+++ weatherCmdLn/weather.js
@@ -6,41 +6,40 @@ const printTemp = (city, temp) => {
 }
 
 const printError = (error) => {
-    console.error(error.message);
+  console.error(error.message);
 }
 
 const getWeather = (zipOrCity, apiKey) => {
     try {
-        let query;
+      let query;
 
-        if (zipOrCity.match(/(^\d{5}$)/gm)) {
-          query = `http://api.openweathermap.org/data/2.5/weather?zip=${zipOrCity}&units=imperial&APPID=${apiKey}`;
-        } else {
-          query = `http://api.openweathermap.org/data/2.5/weather?q=${zipOrCity}&units=imperial&APPID=${apiKey}`;
-        }
-      
+      if (zipOrCity.match(/(^\d{5}$)/gm)) {
+        query = `http://api.openweathermap.org/data/2.5/weather?zip=${zipOrCity}&units=imperial&APPID=${apiKey}`;
+      } else {
+        query = `http://api.openweathermap.org/data/2.5/weather?q=${zipOrCity}&units=imperial&APPID=${apiKey}`;
+      }
+
         const request = http.get(query, (res) => {
           let body = '';
-      
+
           res.on('data', data => {
             body += data.toString();
           })
       
           res.on('end', () => {
             body = JSON.parse(body);
-            try {
+            if (res.statusCode === 200) {
               printTemp(body.name, Math.round(body.main.temp * 10) / 10);
-            } catch (error) {
+            } else {
               console.error(`Error encountered:\n`
               + `Status Code: ${body.cod} \n`
               + `Response Message: ${body.message}`);
             }
           })
         });
-
-        request.on('error', error => console.error(`Problem with request: ${error.message}`));
+      request.on('error', error => console.error(`Problem with request: ${error.message}`));
     } catch (error) {
-        printError(error);
+      printError(error);
     }
 };
 
